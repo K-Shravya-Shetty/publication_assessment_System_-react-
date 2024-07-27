23acc4acc3f5ccc9741b8a10f611278984c02a33
Priyanka S Mayya <priyankasmayya@gmail.com>
2024-07-27 11:48:06 +0530

    added dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -31,10 +31,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Dashboard.css';
 import logo from './images/sjec-logo.png';
+import './css/bootstrap-4.0.0-dist/css/bootstrap.min.css';
 
 const Dashboard = () => {
   return (
+    
     <div className="dashboard-container">
+     <link rel='stylesheet' href='css/bootstrap.min.css'></link>
       <div className="nav">
         <ul id="list">
           <li className="logo">
@@ -51,13 +54,56 @@ const Dashboard = () => {
           </li>
         </ul>
       </div>
-      <div className="widgets">
+      {/* <div className="widgets">
         <div className="widget">
           <Link to="/viewApplication" className="widget-link">
             View Application
           </Link>
         </div>
-      </div>
+      </div> */}
+      <div className="container"> 
+        <div className="row mt-5"> 
+          <div className="col"> 
+            <div className="card mt-5"> 
+              <div className="card-header"> 
+                <h2 className="display-6 text-center">New Applications</h2>
+                 </div> 
+                 <div className="card-body"> 
+                  <table className="table table-bordered text-center"> 
+                    <tr className="bg"> 
+                      <td> Application No. </td> 
+                      <td> Title </td> 
+                      <td> Status </td>
+                       <td> View </td>
+                       <td>Action</td> </tr> 
+                       <tr>
+                        <td>1323</td>
+                        <td>hello</td>
+                        <td>Pending</td>
+                        <td><button>View</button></td>
+                        <td><button>Approve</button></td>
+                       </tr>
+                       </table>
+                      
+                       </div>
+                       <div>
+                        <h2 className='display-6 text-center'>Approved Applications</h2>
+                        <table className="table table-bordered text-center"> 
+                     <tr className="bg"> 
+                      <td> Application No. </td> 
+                      <td> Title </td> 
+                       <td> View </td>
+                       </tr> 
+                       
+                       
+                       
+                       </table></div>
+                       </div>
+
+                       </div>
+                       </div>
+
+    </div>
     </div>
   );
 };
